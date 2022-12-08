e974293e09cddeccfafa7c9851cfc8d6f6a10745
Sang Nguyen <sang.nguyenxuan@igrafx.com>
2022-12-08 16:14:10 +0700

    update upgrade

--- src/main/java/uit/edu/vn/universitymanagement/config/ModelMapperConfig.java
+++ src/main/java/uit/edu/vn/universitymanagement/config/ModelMapperConfig.java
@@ -1,13 +1,9 @@
 package uit.edu.vn.universitymanagement.config;
 
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.Condition;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Configuration;
 import uit.edu.vn.universitymanagement.dto.AccountDto;
-import uit.edu.vn.universitymanagement.dto.MetadataDto;
-import uit.edu.vn.universitymanagement.model.Metadata;
 import uit.edu.vn.universitymanagement.model.entity.Account;
 
 import javax.annotation.PostConstruct;
@@ -16,18 +12,6 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class ModelMapperConfig {
     private final ModelMapper modelMapper;
-
-    @PostConstruct
-    public void metadataToMetadataDto() {
-        TypeMap<Metadata, MetadataDto> typeMap = modelMapper.createTypeMap(Metadata.class, MetadataDto.class);
-        typeMap.addMappings(mapper -> mapper.map(src -> src.getLastModifier().getId(), MetadataDto::setLastModifierId));
-    }
-
-    @PostConstruct
-    public void metadataDtoToMetadata() {
-        modelMapper.createTypeMap(MetadataDto.class, Metadata.class)
-                .addMappings(mapping -> mapping.when((Condition<MetadataDto, Metadata>) context -> false));
-    }
 
     @PostConstruct
     public void accountToAccountDto() {
