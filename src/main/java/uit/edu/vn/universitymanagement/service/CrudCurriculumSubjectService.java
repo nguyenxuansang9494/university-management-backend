afbce48d6af8f81f1d80b42dddede51099a3f68c
MarsLover <MarsLover@NotReal.me>
2022-12-13 02:20:02 +0700

    CurriculumSubjectLogicService

--- src/main/java/uit/edu/vn/universitymanagement/service/CrudCurriculumSubjectService.java
+++ src/main/java/uit/edu/vn/universitymanagement/service/CrudCurriculumSubjectService.java
@@ -7,10 +7,19 @@ import uit.edu.vn.universitymanagement.exception.FailedToDeleteRequiredSubjectEx
 import uit.edu.vn.universitymanagement.exception.MissingPrerequisiteSubjectException;
 import uit.edu.vn.universitymanagement.model.entity.CurriculumSubject;
 import uit.edu.vn.universitymanagement.model.entity.PrerequisiteSubject;
+import uit.edu.vn.universitymanagement.model.entity.Subject;
 import uit.edu.vn.universitymanagement.repository.CommonJpaRepository;
+import uit.edu.vn.universitymanagement.repository.CurriculumSubjectRepository;
 import uit.edu.vn.universitymanagement.repository.PrerequisiteSubjectRepository;
+import uit.edu.vn.universitymanagement.util.ManagedModelUtils;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class CrudCurriculumSubjectService extends AbstractCrudService<CurriculumSubject> {
@@ -51,13 +60,42 @@ public class CrudCurriculumSubjectService extends AbstractCrudService<Curriculum
     }
 
     @Override
+    @Transactional
     public List<CurriculumSubject> create(Authentication authentication, List<CurriculumSubject> objects) {
-        return super.create(authentication, objects);
+        Map<Long, List<CurriculumSubject>> curMap = new HashMap<>();
+        List<CurriculumSubject> result = new LinkedList<>();
+        for (CurriculumSubject object : objects) {
+            if (!curMap.containsKey(object.getCurriculum().getId())) {
+                curMap.put(object.getCurriculum().getId(), new LinkedList<>());
+            }
+            curMap.get(object.getCurriculum().getId()).add(object);
+        }
+        for (Map.Entry<Long, List<CurriculumSubject>> entry : curMap.entrySet()) {
+            List<CurriculumSubject> curSubs = entry.getValue();
+            List<Long> subjectIds = curSubs.stream()
+                    .map(CurriculumSubject::getSubject)
+                    .map(Subject::getId)
+                    .collect(Collectors.toList());
+            Set<PrerequisiteSubject> prerequisiteSubjects = new HashSet<>(prerequisiteSubjectRepository.findAllBySubjectIdIn(subjectIds));
+            List<Long> prerequisiteIds = prerequisiteSubjects.stream()
+                    .map(PrerequisiteSubject::getPrerequisite)
+                    .map(Subject::getId)
+                    .collect(Collectors.toList());
+            List<CurriculumSubject> existedCurSubs = ((CurriculumSubjectRepository) repository).findAllByCurriculumIdAndSubjectIdIn(entry.getKey(), prerequisiteIds);
+            if (!curriculumSubjectLogicService.isAddable(curSubs, existedCurSubs, prerequisiteSubjects))
+                continue;
+            curriculumSubjectLogicService.updatePrerequisiteSubjects(curSubs, existedCurSubs, prerequisiteSubjects);
+            result.addAll(super.create(authentication, curSubs));
+        }
+        return result;
     }
 
     @Override
+    @Transactional
     public List<CurriculumSubject> update(Authentication authentication, List<CurriculumSubject> objects) {
-        return super.update(authentication, objects);
+        List<Long> objectIds = ManagedModelUtils.convertToLongList(objects);
+        delete(authentication, objectIds);
+        return create(authentication, objects);
     }
 
     @Override
--- src/main/java/uit/edu/vn/universitymanagement/service/CurriculumSubjectLogicService.java
+++ src/main/java/uit/edu/vn/universitymanagement/service/CurriculumSubjectLogicService.java
@@ -3,7 +3,6 @@ package uit.edu.vn.universitymanagement.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uit.edu.vn.universitymanagement.authorization.Authorizer;
-import uit.edu.vn.universitymanagement.model.entity.Curriculum;
 import uit.edu.vn.universitymanagement.model.entity.CurriculumSubject;
 import uit.edu.vn.universitymanagement.model.entity.PrerequisiteSubject;
 import uit.edu.vn.universitymanagement.model.entity.Subject;
@@ -12,6 +11,7 @@ import uit.edu.vn.universitymanagement.repository.PrerequisiteSubjectRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -30,27 +30,27 @@ public class CurriculumSubjectLogicService implements Authorizer<CurriculumSubje
         return curriculumSubjectRepository.countAllByCurriculumIdAndSubjectIdIn(curriculumSubject.getCurriculum().getId(), new ArrayList<>(prerequisiteSubjectIds)) == prerequisiteSubjectIds.size();
     }
 
-    public boolean isAddable(List<CurriculumSubject> curriculumSubjects) {
-        Set<Long> addedSubjects = curriculumSubjects.stream()
+    public boolean isAddable(List<CurriculumSubject> addedCurriculumSubjects, List<CurriculumSubject> existedCurSub, Set<PrerequisiteSubject> prerequisiteSubjects) {
+        if (addedCurriculumSubjects.isEmpty())
+            return false;
+        for (CurriculumSubject cur : addedCurriculumSubjects) {
+            if (!Objects.equals(cur.getCurriculum().getId(), addedCurriculumSubjects.get(0).getCurriculum().getId()))
+                return false;
+        }
+        Set<Long> addedSubjects = addedCurriculumSubjects.stream()
                 .map(CurriculumSubject::getSubject)
                 .map(Subject::getId)
                 .collect(Collectors.toSet());
-        Set<Long> prerequisites = prerequisiteSubjectRepository.findAllBySubjectIdIn(new ArrayList<>(addedSubjects))
-                .stream()
+        Set<Long> prerequisites = prerequisiteSubjects.stream()
                 .map(PrerequisiteSubject::getPrerequisite)
                 .map(Subject::getId)
                 .collect(Collectors.toSet());
-        Set<Long> curriculumIds = curriculumSubjects.stream()
-                .map(CurriculumSubject::getCurriculum)
-                .map(Curriculum::getId)
-                .collect(Collectors.toSet());
-        Set<Long> existedInCurSubject = curriculumSubjectRepository.findAllByCurriculumIdInAndSubjectIdIn(new ArrayList<>(curriculumIds), new ArrayList<>(prerequisites))
-                .stream()
+        Set<Long> existedInCurSubIds = existedCurSub.stream()
                 .map(CurriculumSubject::getSubject)
                 .map(Subject::getId)
                 .collect(Collectors.toSet());
         return prerequisites.stream()
-                .map(e -> addedSubjects.contains(e) || existedInCurSubject.contains(e))
+                .map(e -> addedSubjects.contains(e) || existedInCurSubIds.contains(e))
                 .reduce(Boolean::logicalAnd)
                 .orElse(false);
     }
@@ -90,5 +90,41 @@ public class CurriculumSubjectLogicService implements Authorizer<CurriculumSubje
         List<CurriculumSubject> curriculumSubjects = curriculumSubjectRepository.findAllByCurriculumIdAndSubjectIdIn(curriculumSubject.getCurriculum().getId(), new ArrayList<>(prerequisiteIds));
         curriculumSubjects.forEach(e -> e.setOptional(e.isOptional() && curriculumSubject.isOptional()));
         curriculumSubjectRepository.saveAll(curriculumSubjects);
+    }
+
+    public void updatePrerequisiteSubjects(List<CurriculumSubject> curriculumSubjects, List<CurriculumSubject> existedCurSubs, Set<PrerequisiteSubject> prerequisiteSubjectSet) {
+        curriculumSubjects.addAll(existedCurSubs);
+        curriculumSubjects.sort((e1, e2) -> calculateDepth(e2, prerequisiteSubjectSet) - calculateDepth(e1, prerequisiteSubjectSet));
+        for (CurriculumSubject curSub : curriculumSubjects) {
+            Set<Long> dependants = prerequisiteSubjectSet.stream()
+                    .map(PrerequisiteSubject::getPrerequisite)
+                    .map(Subject::getId)
+                    .filter(e -> curSub.getSubject().getId().equals(e))
+                    .collect(Collectors.toSet());
+            curSub.setOptional(curriculumSubjects.stream()
+                    .filter(e -> dependants.contains(e.getSubject().getId()))
+                    .map(CurriculumSubject::isOptional)
+                    .reduce(Boolean::logicalAnd)
+                    .orElse(false));
+        }
+    }
+
+    public int calculateDepth(CurriculumSubject curriculumSubject, Set<PrerequisiteSubject> prerequisiteSubjectSet) {
+        Set<Long> prerequisites = prerequisiteSubjectSet.stream()
+                .filter(e -> Objects.equals(e.getSubject().getId(), curriculumSubject.getSubject().getId()))
+                .map(PrerequisiteSubject::getPrerequisite)
+                .map(Subject::getId)
+                .collect(Collectors.toSet());
+        int depth = 0;
+        while (!prerequisites.isEmpty()) {
+            final Set<Long> constPrerequisites = prerequisites;
+            depth++;
+            prerequisites = prerequisiteSubjectSet.stream()
+                    .filter(e -> constPrerequisites.contains(e.getSubject().getId()))
+                    .map(PrerequisiteSubject::getPrerequisite)
+                    .map(Subject::getId)
+                    .collect(Collectors.toSet());
+        }
+        return depth;
     }
 }
